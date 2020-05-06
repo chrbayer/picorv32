5add89cd85de8cc11b88f5f4f9385b5ea86514dc
Matthieu Michon <matthieu.michon@gmail.com>
2020-05-07 00:46:45 +0200

    Add patched N25Qxxx SPI flash model

---
+++ picosoc/N25Qxxx/include/UserData.h
@@ -0,0 +1,14 @@
+// ************************************ 
+//
+// User Data definition file :
+//
+//      here are defined all parameters
+//      that the user can change
+//
+// ************************************ 
+
+`define N25Q128A13E
+
+`define FILENAME_mem "artya7c_fw.hex" // Memory File Name 
+`define FILENAME_sfdp "sfdp.vmf" // SFDP File Name 
+`define NVCR_DEFAULT_VALUE 'hFFFF
