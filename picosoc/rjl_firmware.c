b753f9ff57579c798bda93e3abcd074be9ecf45f
Joe Loughry <joe.loughry@gmail.com>
2021-01-02 10:41:51 -0700

    activity indicators working

--- picosoc/rjl_firmware.c
+++ picosoc/rjl_firmware.c
@@ -30,10 +30,9 @@ extern uint32_t sram;
 
 // --------------------------------------------------------
 
-extern uint32_t flashio_worker_begin;
-extern uint32_t flashio_worker_end;
+void delay(unsigned long microseconds) {
+	unsigned long delay_in_cycles = microseconds * 12;
 
-void delay() {
 	uint32_t cycles_begin, cycles_now, cycles = 0;
 	__asm__ volatile ("rdcycle %0" : "=r"(cycles_begin));
 	while (cycles < 12000000) {
@@ -46,6 +45,8 @@ void delay() {
 #define report_led (1<<0)
 #define activity_red_led (1<<1)
 #define activity_green_led (1<<2)
+#define red_inbuilt_led (1<<6)
+#define green_inbuilt_led (1<<7)
 
 // The following is mapped to 0x05
 #define sense_led (1<<0)
@@ -92,6 +93,32 @@ void activity_indicator_green() {
 	activity_indicator_red_off();
 }
 
+void green_inbuilt_led_on() {
+	reg_leds |= green_inbuilt_led;
+}
+
+void green_inbuilt_led_off() {
+	reg_leds &= ~green_inbuilt_led;
+}
+
+void red_inbuilt_led_on() {
+	reg_leds |= red_inbuilt_led;
+}
+
+void red_inbuilt_led_off() {
+	reg_leds &= ~red_inbuilt_led;
+}
+
+void inbuilt_activity_indicator_red() {
+	red_inbuilt_led_on();
+	green_inbuilt_led_off();
+}
+
+void inbuilt_activity_indicator_green() {
+	red_inbuilt_led_off();
+	green_inbuilt_led_on();
+}
+
 void all_leds_off() {
 	reg_leds = 0;
 	reg_new_pmod_leds = 0;
@@ -118,11 +145,104 @@ void nonblocking_activity_indicator() {
 			case green:
 				activity_indicator_red();
 				state = red;
+				break;
 			default:
 				state = red;
 				break;
 		}
 		time_of_last_state_change = now;
+	}
+}
+
+void inbuilt_activity_indicator() {
+	static enum states {red, green, invalid_state} state = red;
+	static uint32_t time_of_last_state_change = 0;
+	const unsigned int period = 3300000; // 12 000 000 per second
+	uint32_t now;
+
+	// The cycle counter is only 32 bits and ticks 12 million
+	// times a second, so expect it to roll over about every
+	// five minutes. That's acceptable, for now.
+
+	__asm__ volatile ("rdcycle %0" : "=r"(now));
+	if (now - time_of_last_state_change > period) {
+		switch(state) {
+			case red:
+				inbuilt_activity_indicator_green();
+				state = green;
+				break;
+			case green:
+				inbuilt_activity_indicator_red();
+				state = red;
+				break;
+			default:
+				state = red;
+				break;
+		}
+		time_of_last_state_change = now;
+	}
+}
+
+void nonblocking_1s_counter() {
+	static enum states {on, off, invalid_state} state = off;
+	static uint32_t time_of_last_state_change = 0;
+	uint32_t now;
+	const unsigned long one_second = 12000000; // cycles
+
+	// The cycle counter is only 32 bits and ticks 12 million
+	// times a second, so expect it to roll over about every
+	// five minutes. That's acceptable, for now.
+
+	__asm__ volatile ("rdcycle %0" : "=r"(now));
+	if (now - time_of_last_state_change > one_second) {
+		switch(state) {
+			case on:
+				report_led_off();
+				state = off;
+				break;
+			case off:
+				report_led_on();
+				state = on;
+				break;
+			default:
+				state = on;
+				break;
+		}
+		time_of_last_state_change = now;
+	}
+}
+
+void experiment_with_extremely_short_interval() {
+	static enum states {on, off, invalid_state} state = off;
+	static uint32_t time_of_last_state_change = 0;
+	uint32_t now;
+	const unsigned long one_second = 12000000; // cycles
+	const unsigned long on_time = 12000; // multiplication and div not allowed here
+	const unsigned long off_time = one_second;
+
+	// The cycle counter is only 32 bits and ticks 12 million
+	// times a second, so expect it to roll over about every
+	// five minutes. That's acceptable, for now.
+
+	__asm__ volatile ("rdcycle %0" : "=r"(now));
+	switch(state) {
+		case on:
+			if (now - time_of_last_state_change > on_time) {
+				red_inbuilt_led_off();
+				state = off;
+				time_of_last_state_change = now;
+			}
+			break;
+		case off:
+			if (now - time_of_last_state_change > off_time) {
+				red_inbuilt_led_on();
+				state = on;
+				time_of_last_state_change = now;
+			}
+			break;
+		default:
+			state = on;
+			break;
 	}
 }
 
@@ -134,7 +254,9 @@ void main() {
 	all_leds_off(0);
 	while(1) {
 		nonblocking_activity_indicator();
-		nonblocking_sense_and_report();
+		// experiment_with_extremely_short_interval();
+		nonblocking_1s_counter();
+		// inbuilt_activity_indicator();
 	}
 }
 
